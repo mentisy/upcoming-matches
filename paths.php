e9a4845482f322e7773f53d264812a891ff4fb1a
Alexander Volle <alexander.volle@hotmail.com>
2020-10-02 23:17:58 +0200

    Change name of non-public images folder to renderables, to distinguish the two types

--- paths.php
+++ paths.php
@@ -5,6 +5,6 @@ define('ROOT', __DIR__ . DS);
 define('LOGS', ROOT . 'logs' . DS);
 define('FILES', ROOT . 'files'. DS);
 define('FONTS', ROOT . 'fonts' . DS);
-define('IMAGES', ROOT . 'images' . DS);
+define('RENDERABLES', ROOT . 'renderables' . DS);
 define('RENDERINGS', ROOT . 'renderings' . DS);
 define('TEMPLATES', ROOT . 'templates' . DS);
