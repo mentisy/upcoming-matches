39936ead78c841ad3ce23c13a960cc460bed7dde
Alexander Volle <alexander.volle@hotmail.com>
2020-10-02 22:21:58 +0200

    Improve docblocks

--- src/Match.php
+++ src/Match.php
@@ -2,18 +2,77 @@
 
 namespace Avolle\WeeklyMatches;
 
+use DateTime;
+
+/**
+ * Class Match
+ *
+ * @package Avolle\WeeklyMatches
+ */
 class Match
 {
-    public \DateTime $date;
+    /**
+     * Date of match
+     *
+     * @var \DateTime
+     */
+    public DateTime $date;
+
+    /**
+     * Day of match
+     *
+     * @var string
+     */
     public string $day;
+
+    /**
+     * Time of match
+     *
+     * @var string
+     */
     public string $time;
+
+    /**
+     * Home team of match
+     *
+     * @var string
+     */
     public string $homeTeam;
+
+    /**
+     * Away team of match
+     *
+     * @var string
+     */
     public string $awayTeam;
+
+    /**
+     * Pitch match is played on
+     *
+     * @var string
+     */
     public string $pitch;
+
+    /**
+     * Tournament of match
+     *
+     * @var string
+     */
     public string $tournament;
 
+    /**
+     * Match constructor.
+     *
+     * @param \DateTime $date Date
+     * @param string $day Day
+     * @param string $time Time
+     * @param string $homeTeam Home team
+     * @param string $awayTeam Away team
+     * @param string $pitch Pitch
+     * @param string $tournament Tournament
+     */
     public function __construct(
-        \DateTime $date,
+        DateTime $date,
         string $day,
         string $time,
         string $homeTeam,
--- src/SpreadsheetReader.php
+++ src/SpreadsheetReader.php
@@ -2,15 +2,27 @@
 
 namespace Avolle\WeeklyMatches;
 
-
 use Avolle\WeeklyMatches\Exception\InvalidExcelConfiguration;
 use PhpOffice\PhpSpreadsheet\Exception;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
+/**
+ * Class SpreadsheetReader
+ *
+ * @package Avolle\WeeklyMatches
+ */
 class SpreadsheetReader
 {
+    /**
+     * Options for the spreadsheet reader
+     *
+     * - hasHeaders - bool: Whether the spreadsheet has headers, indicating that data reading should start at row two
+     * - headers - array: Maps the different Match entity properties to columns in the spreadsheet
+     *
+     * @var array
+     */
     protected array $options = [
         'hasHeaders' => true,
         'headers' => [
@@ -24,9 +36,16 @@ class SpreadsheetReader
         ],
     ];
 
+    /**
+     * Spreadsheet instance
+     *
+     * @var \PhpOffice\PhpSpreadsheet\Spreadsheet
+     */
     private Spreadsheet $spreadsheet;
 
     /**
+     * An array of Match entities
+     *
      * @var \Avolle\WeeklyMatches\Match[]
      */
     private array $matches;
@@ -34,8 +53,8 @@ class SpreadsheetReader
     /**
      * SpreadsheetReader constructor.
      *
-     * @param string $filename
-     * @param array $options
+     * @param string $filename Filename to read spreadsheet from
+     * @param array $options Options to use in the reader
      * @throws \Avolle\WeeklyMatches\Exception\InvalidExcelConfiguration
      */
     public function __construct(string $filename, $options = [])
@@ -50,6 +69,8 @@ class SpreadsheetReader
     }
 
     /**
+     * Read matches from the spreadsheet and compile them into an array of Match entities
+     *
      * @return array
      * @throws \Avolle\WeeklyMatches\Exception\InvalidExcelConfiguration
      */
@@ -85,6 +106,11 @@ class SpreadsheetReader
         return $matches;
     }
 
+    /**
+     * Evaluate which row contains data, so the spreadsheet can start reading from that row
+     *
+     * @return int
+     */
     private function firstRow(): int
     {
     	if ($this->options['hasHeaders']) {
@@ -94,17 +120,31 @@ class SpreadsheetReader
     	return 1;
     }
 
+    /**
+     * Evaluate which row is the last to contain data
+     *
+     * @return int
+     */
     private function lastRow(): int
     {
         return $this->spreadsheet->getActiveSheet()->getHighestDataRow();
     }
 
+    /**
+     * Create a cell string to be read, compiled from the input row and column
+     *
+     * @param string $column Column of cell
+     * @param int $row Row of cell
+     * @return string
+     */
     private function cell(string $column, int $row)
     {
         return sprintf("%s%s", $column, $row);
     }
 
     /**
+     * Returns the compiled array of Match entities
+     *
      * @return \Avolle\WeeklyMatches\Match[]
      */
     public function getMatches(): array
