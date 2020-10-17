d5760860d7f9cb45e2ab6fe8a1490293cb8ad8e6
Alexander Volle <alexander.volle@hotmail.com>
2020-10-17 17:32:22 +0200

    Renders now accept a SportConfig instance and can use the data within

--- src/Render/RenderInterface.php
+++ src/Render/RenderInterface.php
@@ -2,7 +2,21 @@
 
 namespace Avolle\UpcomingMatches\Render;
 
+use Avolle\UpcomingMatches\SportConfig;
+use Cake\Collection\CollectionInterface;
+
 interface RenderInterface
 {
+    /**
+     * RenderInterface constructor.
+     *
+     * @param \Cake\Collection\CollectionInterface $matches Matches to render
+     * @param \Avolle\UpcomingMatches\SportConfig $sportConfig The sport's config to retrive relevant info about
+     */
+    public function __construct(CollectionInterface $matches, SportConfig $sportConfig);
+
+    /**
+     * Output method. Will output the render
+     */
     public function output(): void;
 }
